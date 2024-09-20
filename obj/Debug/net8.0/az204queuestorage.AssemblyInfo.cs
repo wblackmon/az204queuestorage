af7819f9f826c690f39f0913d17a55b94c098841
wblackmon <wayne.blackmon@outlook.com>
2024-09-20 14:03:41 -0500

    Lets do this

--- obj/Debug/net8.0/az204queuestorage.AssemblyInfo.cs
+++ obj/Debug/net8.0/az204queuestorage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("az204queuestorage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b6e5eef5f08147fcf1b9c54b05c423167e30edf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec2da9772d48972afe17a3ac14bce3c97a9e7dd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("az204queuestorage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("az204queuestorage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
